13e87f36afccfb6c2b8e989100af4c80e845f3f4
Takeshi Kyogoku <3711700+kyocabu@users.noreply.github.com>
2023-04-20 12:17:05 +0900

    Update Bank.java
    ver up

--- bank/src/main/java/servlet/Bank.java
+++ bank/src/main/java/servlet/Bank.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/Bank")
 public class Bank extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 3L;
        
     /**
      * @see HttpServlet#HttpServlet()
